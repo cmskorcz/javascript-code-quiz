ee75d1f5c826f49785573e904a2e8529372c8fee
cmskorcz <chaseskorey@icloud.com>
2022-04-17 15:45:46 -0400

    questions change after answering, answers change,correct/incorrect changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,17 +6,27 @@ const answerBtnEl = document.querySelector('.answer-btn')
 // Initialize timer, intervalID, and score
 let timeCount = 75
 let intervalID;
-let scoreCount = 0
+let scoreCount = 0;
 
 // Question Array
-const question = {
-    heading: "This is a question?",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-    answer: "Answer 3"
-}
-
+const question = [
+    {
+        heading: "This is a question?",
+        options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        answer: "Answer 3"
+    },
+    {
+        heading: "This is also a question?",
+        options: ["Answer 5", "Answer 6", "Answer 7", "Answer 8"],
+        answer: "Answer 5"
+    }
+]
+// Initialize question idex counter
+let questionIndex;
 
 const createQuestion = () => {
+    questionIndex = 0;
+
     const questionContainerEl = document.createElement('div');
     const questionHeadingEl = document.createElement('h2');
     const answerContainerEl = document.createElement('div');
@@ -24,17 +34,24 @@ const createQuestion = () => {
     questionContainerEl.className = "container-flex";
     answerContainerEl.className = "answer-flex";
 
-    questionHeadingEl.textContent = question.heading;
+    questionHeadingEl.textContent = question[questionIndex].heading;
 
-    mainEl.replaceWith(questionContainerEl)
+    mainEl.replaceWith(questionContainerEl);
 
     questionContainerEl.appendChild(questionHeadingEl);
 
-    generateAnswers(answerContainerEl);
+    generateAnswers(answerContainerEl, questionIndex);
 
     questionContainerEl.appendChild(answerContainerEl);
 
     startTimer();
+};
+
+const nextQuestion = (index) => {
+    const questionHeadingEl = document.querySelector('h2');
+
+    questionHeadingEl.textContent = question[index].heading;
+    replaceAnswers(question[index]);
 };
 
 const startTimer = () => {
@@ -45,13 +62,13 @@ const stopTimer = () => {
     clearInterval(intervalID);
 };
 
-const generateAnswers = (container) => {
-
-    for (let i = 0; i < question.options.length; i++) {
+const generateAnswers = (container, index) => {
+    let answerArray = question[index].options
+    for (let i = 0; i < answerArray.length; i++) {
         let answer = document.createElement('button');
         answer.className = "btn answer-btn";
-        answer.textContent = question.options[i];
-        answer.setAttribute('value', question.options[i]);
+        answer.textContent = answerArray[i];
+        answer.setAttribute('value', answerArray[i]);
         answer.setAttribute('data-id', i);
 
         answer.addEventListener('click', checkAnswer);
@@ -60,6 +77,16 @@ const generateAnswers = (container) => {
         
     };
 };
+
+const replaceAnswers = (index) => {
+    let answerArray = index.options
+    for (let i = 0; i < answerArray.length; i++) {
+        let answer = document.querySelector(`button[data-id="${i}"]`);
+        answer.textContent = answerArray[i];
+        answer.setAttribute('value', answerArray[i]);
+        answer.setAttribute('data-id', i);
+    }
+}
 
 const postTime = () => {
     const timerSpanEl = document.getElementById("time");
@@ -75,19 +102,27 @@ const postTime = () => {
 
 const checkAnswer = (event) => {
     let answer = event.target;
+
     const containerEl = document.querySelector('.container-flex')
-    const feedbackEl = document.createElement('div');
-    feedbackEl.className = "feedback-flex"
+    let feedbackEl = document.querySelector('div[class="feedback-flex"]');
     
-    if (answer.value === question.answer) {
+    if(!feedbackEl) {
+        feedbackEl = document.createElement('div');
+        feedbackEl.className = "feedback-flex"
+        containerEl.appendChild(feedbackEl);
+    };
+
+    if (answer.value === question[questionIndex].answer) {
         feedbackEl.innerHTML = "<h3>Correct!</h3>";
         scoreCount = scoreCount + 10;
+        questionIndex++;
     } else {
         feedbackEl.innerHTML = "<h3>Incorrect!</h3>";
         timeCount = timeCount - 5;
+        questionIndex++;
     };
-
-    containerEl.appendChild(feedbackEl);
+    
+    nextQuestion(questionIndex);
 };
 
 const endQuiz = () => {
