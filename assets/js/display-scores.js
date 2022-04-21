1913b50313b4602fe672a07dbb3e2e2259c9fc61
cmskorcz <chaseskorey@icloud.com>
2022-04-21 11:33:07 -0400

    added more styles and clear localstorage func

--- assets/js/display-scores.js
+++ assets/js/display-scores.js
@@ -1,14 +1,24 @@
+const clearScoreEl = document.getElementById('clear-score')
+
 const displayScores = () => {
     let scoresJSON = localStorage.getItem('scores');
     let scoresArray = JSON.parse(scoresJSON);
 
     let highscoreListEl = document.getElementById('highscores');
 
-    for (score of scoresArray) {
-        let scoreEl = document.createElement('li');
-        scoreEl.textContent = score;
-        highscoreListEl.appendChild(scoreEl);
+    if (scoresArray) {
+        for (score of scoresArray) {
+            let scoreEl = document.createElement('li');
+            scoreEl.textContent = score;
+            highscoreListEl.appendChild(scoreEl);
+        }    
     }
 }
 
+const clearLocalStorage = () => {
+    localStorage.clear();
+    location.reload();
+}
+
+clearScoreEl.addEventListener('click', clearLocalStorage);
 displayScores();
